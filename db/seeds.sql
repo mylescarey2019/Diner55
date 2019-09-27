9e69f1694ac9466198cce2dc7ede24d50c01abf5
Myles Carey <seanmyles@comcast.net>
2019-09-27 13:33:39 -0700

    finished styling

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,10 +3,16 @@ INSERT INTO food_server (food_server_name)
 VALUES('Logan'),
       ('Ella'),
       ('Nathan'),
+      ('Brittney'),
+      ('Zach'),
+      ('Sara'),
       ('Leah');
       
 -- insert food orders
 INSERT INTO food_order (food_server_id,food_order_name)
-VALUES (1,"BLT with salad"),
+VALUES (1,"BLT"),
        (1,"Turkey Club"),
-       (2,"Cheesburger no onions");
+       (2,"Calzone"),
+       (4,"Tomato Soup"),
+       (4,"Roast Beef with fries"),
+       (5,"Cheesburger no onions");
